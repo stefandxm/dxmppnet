784f38c23158c6c1a3b08f6864aa142b47033cdf
megadeluxe 2000 <stefan@clayster.com>
2017-05-22 13:28:10 +0200

    nuspecjox

--- dxmppnet/dxmppnet/packnuget.sh
+++ dxmppnet/dxmppnet/packnuget.sh
@@ -1,2 +1,8 @@
-nuget pack dxmppnet.nuspec -properties Configuration:Release -symbols
+rm *.nupkg
+rm -rf obj/*
+rm -rf Debug/*
+rm -rf Release/*
+set -e
+msbuild dxmppnet.csproj /p:Configuration=Release /p:TargetFramework=v4.0
+nuget pack dxmppnet.nuspec -properties Configuration:Release
 
